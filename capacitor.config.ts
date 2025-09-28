82dd8a309c7b8a28361d4057875c87c505872b27
Khan tran <kcoder282@gmail.com>
2025-09-29 01:40:03 +0700

    push

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.nguyenvanthoai.edupath',
+  appName: 'Edupath',
+  webDir: 'dist'
+};
+
+export default config;
--- vite.config.ts
+++ vite.config.ts
@@ -23,6 +23,7 @@ export default defineConfig({
       },
     }),
   ],
+  build: { outDir: 'dist', emptyOutDir: true },
   resolve: {
     alias: [
       {
